4146ca6283988671ae9cf29c7e4b030f95c40b5a
Richard Cheng <rhocheng@g.ucla.edu>
2020-03-28 09:32:06 -0700

    Added ChangeScene script

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+	[SerializeField] private string sceneName; // Name of the scene that the button will change to
+
+    // Start is called before the first frame update
+    void Start() {
+        gameObject.GetComponent<Button>().onClick.AddListener(Load); // At the start, make the button call load() when clicked
+    }
+
+    void Load() {
+        SceneManager.LoadScene(name); // Switch to the scene with the correct name
+    }
+}
