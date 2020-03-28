c577ca6663215e7d9e434f81287c68d42fc20242
Richard Cheng <rhocheng@g.ucla.edu>
2020-03-28 11:41:38 -0700

    Added title screen music, camera movement

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+	[SerializeField] private float speed = 1.0f;
+
+    void Update() {
+     if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < 2.5)
+         transform.Translate(new Vector3(speed * Time.deltaTime,0,0));
+     if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > -2.5)
+         transform.Translate(new Vector3(-speed * Time.deltaTime,0,0));
+     if (Input.GetKey(KeyCode.DownArrow) && transform.position.z > -2.5)
+         transform.Translate(new Vector3(0,-speed * Time.deltaTime,0));
+     if (Input.GetKey(KeyCode.UpArrow) && transform.position.z < 2.5)
+         transform.Translate(new Vector3(0,speed * Time.deltaTime,0));
+     }
+}
