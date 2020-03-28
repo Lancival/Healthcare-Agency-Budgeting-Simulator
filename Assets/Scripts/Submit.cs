09f69dab3d00a411773e5242d278e58d2e80fcc0
Richard Cheng <rhocheng@g.ucla.edu>
2020-03-28 15:03:50 -0700

    Submit script created

---
+++ Assets/Scripts/Submit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Submit : MonoBehaviour {
+	private Button b;
+
+    void Start() {
+    	Globals.starting_budget = Globals.original_budget;
+    	b = transform.GetComponent<Button>();
+    	b.onClick.AddListener(SubmitBudget);
+    	b.interactable = false;
+    }
+
+    void Update() {
+    	if (Globals.starting_budget >= Globals.Total())
+    		b.interactable = true;
+    }
+
+    void SubmitBudget() {
+    	Globals.quarter++;
+    	Globals.starting_budget = (int) (Globals.starting_budget * (1.0f - (float)(Globals.difficulty) / 10.0f));
+    	UpdateLetter();
+    }
+
+    void UpdateLetter() {
+    	// Update the contents of the letter to reflect your decisions
+    	return;
+    }
+}
