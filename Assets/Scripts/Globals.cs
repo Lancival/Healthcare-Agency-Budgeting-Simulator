2cd4a356f4903a366475a4149ea8515681dd3b7b
Richard Cheng <rhocheng@g.ucla.edu>
2020-03-28 10:56:27 -0700

    Added globals script

---
+++ Assets/Scripts/Globals.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+static class Globals {
+	public static float volume = 1.0f;
+	public static int difficulty = 1; // 0,1,2 = easy,normal,hard?
+	public static int original_budget = 7178; // Starting budget of quarter 1
+	public static int starting_budget; // Maximum amount that can be spent this quarter
+	public static int current_budget; // Current amount being spent
+	public static int quarter = 1; // Current quarter
+}
